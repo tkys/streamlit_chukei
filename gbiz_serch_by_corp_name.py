ee5981a0d92cde90817b85d0a3f1005b1545e960
tkys <tkysyskw@gmail.com>
2022-03-10 18:30:47 +0900

    Update gbiz_serch_by_corp_name.py

--- gbiz_serch_by_corp_name.py
+++ gbiz_serch_by_corp_name.py
@@ -3,6 +3,11 @@ import streamlit as st
 import requests
 import json
 
+import time
+def now_time():
+    now = time.localtime()
+    d = time.strftime('%Y%m%d%H%M%S', now)  # YYYYMMDDhhmmssに書式化
+    return d
 
 #for search
 endpoint_url = "https://info.gbiz.go.jp/api/v1/hojin"
@@ -15,41 +20,65 @@ headers = {
             "X-hojinInfo-api-token": token
         }
 
+token = "M3zZzZx8sFaGeQVY78PHgHCliCCIZT8M"
+
+headers = {
+            "Accept": "application/json",
+            "X-hojinInfo-api-token": token
+        }
+
 def search_by_name(corp_name:str):
     params = {"name":corp_name}
     res = requests.get(
-        url = endpoint_url,
+        url     = endpoint_url,
         headers = headers,
-        params = params
+        params  = params
         )
     #json = res.json()#['hojin-infos']
     #print(json)
     #res.json()でJSONデータに変換して変数へ保存
     json_data = res.json()
-    #print(json_data['hojin-infos'])
+    #print(len(json_data['hojin-infos']))
+    #print(json_data['hojin-infos'][1])
     #取得したJSONデータが単一のJSONオブジェクトではなく
     #配列みたいになっているときはfor文と組み合わせてやるとよし。
-    return_dic= {
-        'name': "",
-        'location': "",
-        'corporate_number': "",
-        "update_date":""
-        }
+
+    return_list =[]
+
     #print(json_data["message"])
-    for jsonObj in json_data['hojin-infos']:
-        #print(jsonObj["corporate_number"],jsonObj["name"],jsonObj["location"])        
-        return_dic["name"] = jsonObj["name"]
-        return_dic["location"] = jsonObj["location"]
-        return_dic["corporate_number"] = jsonObj["corporate_number"]
-        return_dic["update_date"] = jsonObj["update_date"]
-    
-    return return_dic#json_data['hojin-infos']
+
+    if json_data["message"] !=  "200 - OK.":
+        result = {"status":json_data["message"], "body":""}
+        
+    else:
+        for i in range(len(json_data['hojin-infos'])):
+
+            #print(json_data['hojin-infos'][0]["name"])
+            return_dic= {
+           'search_id'         : "",
+            'name'              : "",
+            'location'          : "",
+            'corporate_number'  : "",
+            "update_date"       : ""
+            }
+
+            return_dic["search_id"]         = str(now_time()) + "_" + str(i)
+            return_dic["name"]              = json_data['hojin-infos'][i]["name"]
+            return_dic["location"]          = json_data['hojin-infos'][i]["location"]
+            return_dic["corporate_number"]  = json_data['hojin-infos'][i]["corporate_number"]
+            return_dic["update_date"]       = json_data['hojin-infos'][i]["update_date"]
+
+            return_list.append(return_dic)
+
+        result = {"status":json_data["message"], "body":json.dumps(return_list, ensure_ascii = False)}
+        #result = return_list
+     
+    return result
    
 
 def main():
-    #print(search_by_name("スペース・アイ株式会社"))
-    st.write(search_by_name("スペース・アイ株式会社"))
-    
-    
+    #print(search_by_name("株式会社スペース・アイ"))
+    st.write(search_by_name("スペース・アイ"))
+        
 if __name__ == '__main__':
     main()
