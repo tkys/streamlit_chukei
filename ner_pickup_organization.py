6db463865367b51f813eed44d7703b0706c7bf09
tkys <tkysyskw@gmail.com>
2022-03-14 10:31:49 +0900

    Update ner_pickup_organization.py

--- ner_pickup_organization.py
+++ ner_pickup_organization.py
@@ -39,6 +39,8 @@ if check1:
     st.info("Code is analyzing your text.")
     response = req(text_input)
     #print(response)
+    st.text("result : " +  str(len(response["hojin-infos"])) )
+
     st.info(response)
     st.write(pd.DataFrame(response))
 
